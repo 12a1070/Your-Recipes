74572637ca22cd8b517d78a1fc1ca61cef5d04e4
teraitomoya <12a1070.sit.shonan@gmail.com>
2023-10-28 11:16:12 +0900

    devise日本語化

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module YourRecipes
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.i18n.default_locale = :ja
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -20,4 +21,3 @@ module YourRecipes
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
-config.i18n.default_locale = :ja
