c50fe3a4b0249a050b8633da0a95c85c26f4a568
teraitomoya <12a1070.sit.shonan@gmail.com>
2023-11-04 23:28:15 +0900

    投稿機能バリデーション。エラーメッセージ完成

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,9 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   mount_uploader :photo, PhotoUploader
+  validates :photo, presence: true
   validates :title, presence: true
   validates :material, presence: true
   validates :process, presence: true
-
 
 end
