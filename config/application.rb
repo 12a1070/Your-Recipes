36f1eb0c382bb231acbd9bee1804384feb819eb0
teraitomoya <12a1070.sit.shonan@gmail.com>
2023-11-08 16:09:51 +0900

    投稿日表示

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module YourRecipes
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.i18n.default_locale = :ja
+    config.time_zone = 'Asia/Tokyo'
+    config.paths.add 'lib', eager_load: true
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
 
     # Configuration for the application, engines, and railties goes here.
     #
