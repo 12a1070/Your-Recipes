9f65519a4301d0645071cbdd936f3905b3783361
teraitomoya <12a1070.sit.shonan@gmail.com>
2023-11-10 16:15:01 +0900

    Application Controller Blank Deletion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   # deviseの処理を行う場合、devise_parameter_sanitizer.permit(:sign_up,keys:[:email])を実行
 
-
 private
 
     def configure_permitted_parameters
