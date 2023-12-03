a878cd222b96bb8fb0d2bb5eaa2aaf9548c11b7b
teraitomoya <12a1070.sit.shonan@gmail.com>
2023-12-04 07:54:41 +0900

    レシピ削除機能がパス

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
-  belongs_to :user,dependent: :destroy
+  belongs_to :user
   mount_uploader :photo, PhotoUploader
   validates :title, presence: true
   validates :material, presence: true
