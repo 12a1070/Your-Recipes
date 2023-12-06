4fcf245ba2e64b47cc2a3d5a8e8fc91f8368ac7b
teraitomoya <12a1070.sit.shonan@gmail.com>
2023-12-06 18:52:46 +0900

    ルーティング定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'recipes#index'
   resources :recipes
   devise_for :users
+  get "search" => "searches#search"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
