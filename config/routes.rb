d4690174a078f49f57cb4d009b9c124a112c73af
teraitomoya <12a1070.sit.shonan@gmail.com>
2023-12-09 11:13:52 +0900

    不要なsearchのルート削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'recipes#index'
   resources :recipes
   devise_for :users
-  get "search" => "searches#search"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
