4472576c613f61fd8e963cb85b8167b3a6fc8657
teraitomoya <12a1070.sit.shonan@gmail.com>
2023-12-09 20:23:42 +0900

    レシピ削除されたらお気に入りも解除

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy #レシピ削除されたらお気に入りも解除
   mount_uploader :photo, PhotoUploader
   validates :title, presence: true
   validates :material, presence: true
