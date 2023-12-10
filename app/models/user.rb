6a25956ae7701a44d6a927ff15c4cab28f91e3b4
teraitomoya <12a1070.sit.shonan@gmail.com>
2023-12-11 08:17:43 +0900

    Completed 401 Unauthorizedエラー

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :recipes,dependent: :destroy
-  has_many :favorites, # ユーザー/お気に入り → 1:多
+  has_many :favorites # ユーザー/お気に入り → 1:多
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
