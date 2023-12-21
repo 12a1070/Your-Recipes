82b34fb7ac1747daa3f195a69036a887ba11ce41
teraitomoya <12a1070.sit.shonan@gmail.com>
2023-12-21 10:20:42 +0900

    12.21シンタックスエラー

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -12,31 +12,28 @@ class FavoritesController < ApplicationController
 
   # お気に入り登録
   def create
-    if current_user.nil?
-      # ログインしていない場合の処理
-      redirect_to root_path unless current_user
-      #ログインしていたら
-      @favorite = current_user.favorites.build(favorite_params)
-      @recipe = @favorite.recipe
+    # current_user.nil?
+    redirect_to recipes_path unless current_user
+    @favorite = current_user.favorites.build(favorite_params)
+    @recipe = @favorite.recipe
       if @favorite.valid?
-        @favorite.save
-        redirect_to recipe_path(@recipe)
+      @favorite.save
+      redirect_to recipe_path(@recipe)
       end
-    end
   end
 
   # # お気に入り削除
   def destroy
-    if current_user.nil?
-      # ログインしていない場合の処理
-      redirect_to root_path unless current_user # レシピ一覧画面にリダイレクト
-      # ログインされているならレシピ一覧へ遷移
-      @favorite = Favorite.find(params[:id])
-      @recipe = @favorite.recipe
+    current_user.nil?
+    # ログインしていない場合の処理
+    redirect_to root_path unless current_user # レシピ一覧画面にリダイレクト
+    #ログインされているならレシピ一覧へ遷移
+    @favorite = Favorite.find(params[:id])
+    @recipe = @favorite.recipe
       if @favorite.destroy
-        redirect_to recipe_path(@recipe)
+      redirect_to recipe_path(@recipe)
       else
-        redirect_to recipes_path
+      redirect_to recipes_path
       end
     end
   end
