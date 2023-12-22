678dfdf70b5ad945a6c0b2bff3604137d74cf39b
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2023-12-23 07:15:08 +0900

    refactor: ユーザーがログインしていないときの挙動をbefore_actionにまとめる

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,46 +1,38 @@
 class FavoritesController < ApplicationController
+  before_action :redirect_to_root_path, only: %i(index create destroy)
 
   def index
-    if current_user.nil?
-      # ログインしていない場合の処理
-      redirect_to recipes_path
-    else
-    # ログインユーザーがお気に入りに登録されているレシピを全て取得
-      @favorite_recipes = current_user.favorites.map(&:recipe)
-    end
+    @favorite_recipes = current_user.favorites.map(&:recipe)
   end
 
   # お気に入り登録
   def create
-    # current_user.nil?
-    redirect_to recipes_path unless current_user
     @favorite = current_user.favorites.build(favorite_params)
     @recipe = @favorite.recipe
-      if @favorite.valid?
+    if @favorite.valid?
       @favorite.save
       redirect_to recipe_path(@recipe)
-      end
+    end
   end
 
-  # # お気に入り削除
+  # お気に入り削除
   def destroy
-    current_user.nil?
-    # ログインしていない場合の処理
-    redirect_to root_path unless current_user # レシピ一覧画面にリダイレクト
-    #ログインされているならレシピ一覧へ遷移
     @favorite = Favorite.find(params[:id])
     @recipe = @favorite.recipe
-      if @favorite.destroy
+    if @favorite.destroy
       redirect_to recipe_path(@recipe)
-      else
+    else
       redirect_to recipes_path
-      end
+    end
   end
-
 
   private
-    def favorite_params
-      params.permit(:recipe_id)
+  
+  def favorite_params
+    params.permit(:recipe_id)
   end
 
+  def redirect_to_root_path
+    redirect_to root_path if !current_user
+  end
 end
